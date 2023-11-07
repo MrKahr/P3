6ffcd8186b339aaa17e31e4e50231ec2c4ba4213
Frederik <frederik@pretzmann.dk>
2023-11-07 15:04:32 +0100

    Added placeholder methods + formatting

--- BackEndNServer/src/main/java/com/proj/function/AccountManager.java
+++ BackEndNServer/src/main/java/com/proj/function/AccountManager.java
@@ -2,7 +2,6 @@ package com.proj.function;
 
 import com.proj.model.users.*;
 
-
 /**
  * Class responsible for handling all user management except assigning roles
  */
@@ -10,7 +9,7 @@ public class AccountManager {
     // Field
     private Integer numberOfAccounts;
 
-    // Constructor 
+    // Constructor
     public AccountManager(Integer numberOfAccounts) {
         this.numberOfAccounts = numberOfAccounts;
     }
@@ -24,16 +23,17 @@ public class AccountManager {
         this.numberOfAccounts = numberOfAccounts;
     }
 
-    public Guest createAccount(String userName, String password, boolean captcha, boolean requestMembership){        
+    public Guest createAccount(String userName, String password, boolean captcha, boolean requestMembership) {
         try {
             validateCreation(userName, captcha);
         } catch (invalidCaptchaException invlce) {
             // TODO: Send message to frontend: your captcha is wrong.
-        }
-        catch (invalidLoginException invlle) {
+        } catch (invalidLoginException invlle) {
             // TODO: Send message to frontend: your username or password is incorrect.
         }
-        if(requestMembership){requestMembership(userName);}
+        if (requestMembership) {
+            requestMembership(userName);
+        }
 
         Guest guest = new Guest(userName, password);
         this.numberOfAccounts++; // Increment number of accounts since we just created one.
@@ -41,42 +41,42 @@ public class AccountManager {
     }
 
     /**
-     * Ensures the username of the user account that is to be created does not already exist. 
+     * Ensures the username of the user account that is to be created does not
+     * already exist.
+     * 
      * @param userName Display name of user.
      * @param password Password of user.
-     * @param captcha Boolean showing if captcha was correct or not.
+     * @param captcha  Boolean showing if captcha was correct or not.
      * @return True if the username does not exist and the captcha is correct.
-     * @throws invalidLoginException When the login request either had wrong username or password.
+     * @throws invalidLoginException   When the login request either had wrong
+     *                                 username or password.
      * @throws invalidCaptchaException When the captcha wasn't solved correctly.
      */
-    public boolean validateCreation(String userName, boolean captcha) throws invalidLoginException, invalidCaptchaException {
+    public boolean validateCreation(String userName, boolean captcha)
+            throws invalidLoginException, invalidCaptchaException {
         boolean isvalid = false;
         // TODO: We could perform user input validation of username here
-        if(captcha){
+        if (captcha) {
             try {
                 lookupAccount(userName, false);
             } catch (userNotFoundException usrnfe) {
                 // TODO: throw new invalidLoginException()
             }
-        }
-        else {/*TODO: throw new invalidCaptchaException*/}
+        } else {
+            /* TODO: throw new invalidCaptchaException */}
         isvalid = true;
         return isvalid;
     }
 
     /**
-     * Makes a request for the username to the database. Or all users in the database if "all" is true.
+     * Makes a request for the specified username to the database.
      * @param userName Display name of the user.
-     * @param all If true, return all users in the database.
-     * @return User object(s).
+     * @return User object.
      * @throws userNotFoundException When a username is not found in the database.
      */
-    public User lookupAccount(String userName, boolean all) throws userNotFoundException {
-        if(all) {
+    public User lookupAccount(String userName) throws userNotFoundException {
         // TODO: Request to database goes here
-            // Return all users
-        }
-        // TODO: Request to database goes here
+        
         if(request = 404){ 
             // TODO: throw new userNotFoundException()
         }
@@ -86,31 +86,55 @@ public class AccountManager {
     }
 
     /**
-     * Checks login requests to ensure that the captcha and credentials of the user requesting login exist and are correct. 
+     * Retrieves all user accounts from the database. Username only.
+     * 
+     * @return
+     */
+    public Object getAccountList() {
+        // TODO: Request to database goes here
+
+        // Check return type of database. if singular, wrap all users in a list or
+        // something
+        // return all_users;
+    }
+
+    /**
+     * Checks login requests to ensure that the captcha and credentials of the user
+     * requesting login exist and are correct.
+     * 
      * @param userName Display name of user.
      * @param password Password of user.
-     * @param captcha Boolean showing if captcha was correct or not.
+     * @param captcha  Boolean showing if captcha was correct or not.
      * @return True if login request is legitimate (valid credentials and captcha).
-     * @throws invalidLoginException When the login request either had wrong username or password.
+     * @throws invalidLoginException   When the login request either had wrong
+     *                                 username or password.
      * @throws invalidCaptchaException When the captcha wasn't solved correctly.
      */
-    public boolean validateLogin(String userName, String password, boolean captcha) throws invalidLoginException, invalidCaptchaException {
+    public boolean validateLogin(String userName, String password, boolean captcha)
+            throws invalidLoginException, invalidCaptchaException {
         boolean isvalid = false;
-        if(captcha){
+        if (captcha) {
             try {
                 lookupAccount(userName, false);
             } catch (userNotFoundException usrnfe) {
                 // TODO: throw new invalidLoginException()
             }
-        }
-        else {/*TODO: throw new invalidCaptchaException*/}
+        } else {
+            /* TODO: throw new invalidCaptchaException */}
         isvalid = true;
         return isvalid;
     }
 
+    public void requestMembership(String userName) {
+    }
 
-    public void requestMembership(String userName){}
+    public boolean manageAccessLevel(String userName) {
+    }
 
+    public void deactivateAccount(String userName) {
+    }
 
+    public void removeAccount(String userName) {
+    }
 
 }
