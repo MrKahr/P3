929a72fce1d4d82cbb1e820bc7b3a075a4b45ca8
emils <esla22@student.aau.dk>
2023-11-17 15:07:13 +0100

    tilføjede support for at checke rollers dependencies og ændrede på setRole så den thrower hvis der mangler en dependency. Tilføjede også en ny, mere strømlinet constructor til User og et field i Role-klassen som fortæller om rollen er aktiv, så vi ikke behøver at slette en rolle, som en bruger ikke skal have mere

--- BackEndNServer/src/test/java/com/proj/unitTest/roleAssignmentTest.java
+++ BackEndNServer/src/test/java/com/proj/unitTest/roleAssignmentTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.proj.model.users.*;
 import com.proj.model.events.RoleChanged;
@@ -20,8 +21,50 @@ public class roleAssignmentTest {
         assertNotNull(user.getBasicUserInfo()); //is the basic user info present?
     }
 
+    @Test   //we'll be using RoleAssigner for these next tests
+    public void checkForDependencies(){ 
+        User user = new User("user", "1234");   //this user is not an admin or a member
+
+        SuperAdmin superAdmin = new SuperAdmin(); //this role has both admin and member as dependencies
+        assertFalse(RoleAssigner.dependenciesFulfilled(user, superAdmin));
+
+        RoleAssigner.setRole(user, new Guest(""));  //adding the dependency for the member role
+        Member member = new Member("John Doe", "12345678", "0000", "City Town Street 1", "RealMail@mailService.com");
+        assertTrue(RoleAssigner.dependenciesFulfilled(user, member));   //dependencies for member should be fulfilled now
+
+        RoleAssigner.setRole(user, member); //adding member
+        assertFalse(RoleAssigner.dependenciesFulfilled(user, superAdmin));    //still missing a dependency for superAdmin
+
+        RoleAssigner.setRole(user, new Admin(null, null));  //adding admin
+        assertTrue(RoleAssigner.dependenciesFulfilled(user, superAdmin));   //dependencies should be fulfilled now
+        
+        //defining a new role class
+        class BadRole extends Role {
+
+            @Override
+            public RoleType getRoleType() {
+                return RoleType.NOTYPE;
+            }
+
+            @Override
+            public RoleType[] getRoleDependencies() {
+                RoleType[] types = {RoleType.NOTYPE};   //this type is no accounted for in getRoleByType
+                return types;
+            }
+        }
+
+        BadRole badRole = new BadRole();
+
+        Throwable thrown = assertThrows(
+           IllegalArgumentException.class,
+           () -> RoleAssigner.dependenciesFulfilled(user, badRole),
+           "Expected getRoleByType() to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("roleType not recognized!"));
+    }
+
     @Test
-    public void assignNewRole(){    //we'll be using RoleAssigner for these next tests
+    public void setNewRole(){
         BasicUserInfo info = new BasicUserInfo("user1", "1234");
         User user = new User(info);
         Guest guestObject = new Guest("Some information about the user's character");
@@ -35,7 +78,7 @@ public class roleAssignmentTest {
     }
 
     @Test
-    public void replaceRole(){
+    public void setReplacementRole(){
         BasicUserInfo info = new BasicUserInfo("user1", "1234");
         User user = new User(info);
         Guest guestObject = new Guest("Some information about the user's character");
@@ -57,12 +100,12 @@ public class roleAssignmentTest {
         BasicUserInfo info = new BasicUserInfo("user1", "1234");
         User user = new User(info);
 
-        //defining a new role class
+        //defining a new role class with a type that's not accounted for
         class BadRole extends Role {
 
             @Override
             public RoleType getRoleType() {
-                return RoleType.NOTYPE;    //type returned is not accounted for in RoleAssigner
+                return RoleType.NOTYPE;    //type returned is not accounted for in setRole
             }
 
             @Override
@@ -79,7 +122,18 @@ public class roleAssignmentTest {
            () -> RoleAssigner.setRole(user, badRole),
            "Expected setRole() to throw, but it didn't"
         );
-
         assertTrue(thrown.getMessage().contains("newRoleObject returns unknown role type"));    //we want this exact error message
+    }
+
+    @Test
+    public void setRoleMissingDependency(){
+        User user = new User("user", "1234");
+        Member member = new Member("John Doe", "12345678", "0000", "City Town Street 1", "RealMail@mailService.com");
+        Throwable thrown = assertThrows(
+           IllegalArgumentException.class,
+           () -> RoleAssigner.setRole(user, member),
+           "Expected setRole() to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("newRoleObject is missing dependencies!"));
     }
 }
