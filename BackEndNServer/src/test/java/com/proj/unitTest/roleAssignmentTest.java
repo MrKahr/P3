14f1a0465f1a841dcce294bab7ac7364c9056535
emils <esla22@student.aau.dk>
2023-11-20 10:49:27 +0100

    forbedrede tests og implementerede forslag fra tidligere commit

--- BackEndNServer/src/test/java/com/proj/unitTest/roleAssignmentTest.java
+++ BackEndNServer/src/test/java/com/proj/unitTest/roleAssignmentTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.proj.model.users.*;
@@ -28,14 +31,14 @@ public class roleAssignmentTest {
         SuperAdmin superAdmin = new SuperAdmin(); //this role has both admin and member as dependencies
         assertFalse(RoleAssigner.dependenciesFulfilled(user, superAdmin));
 
-        RoleAssigner.setRole(user, new Guest(""));  //adding the dependency for the member role
+        user.setGuestInfo(new Guest(""));  //adding the dependency for the member role
         Member member = new Member("John Doe", "12345678", "0000", "City Town Street 1", "RealMail@mailService.com");
         assertTrue(RoleAssigner.dependenciesFulfilled(user, member));   //dependencies for member should be fulfilled now
 
-        RoleAssigner.setRole(user, member); //adding member
+        user.setMemberInfo(member); //adding member
         assertFalse(RoleAssigner.dependenciesFulfilled(user, superAdmin));    //still missing a dependency for superAdmin
 
-        RoleAssigner.setRole(user, new Admin(null, null));  //adding admin
+        user.setAdminInfo(new Admin(null, null));  //adding admin
         assertTrue(RoleAssigner.dependenciesFulfilled(user, superAdmin));   //dependencies should be fulfilled now
         
         //defining a new role class
@@ -55,11 +58,8 @@ public class roleAssignmentTest {
 
         BadRole badRole = new BadRole();
 
-        Throwable thrown = assertThrows(
-           IllegalArgumentException.class,
-           () -> RoleAssigner.dependenciesFulfilled(user, badRole),
-           "Expected getRoleByType() to throw, but it didn't"
-        );
+        Executable e = () -> {RoleAssigner.dependenciesFulfilled(user, badRole);};
+        Throwable thrown = assertThrows(IllegalArgumentException.class, e, "Expected getRoleByType() to throw, but it didn't");
         assertTrue(thrown.getMessage().contains("roleType not recognized!"));
     }
 
@@ -117,11 +117,8 @@ public class roleAssignmentTest {
 
         BadRole badRole = new BadRole();
 
-        Throwable thrown = assertThrows(
-           IllegalArgumentException.class,
-           () -> RoleAssigner.setRole(user, badRole),
-           "Expected setRole() to throw, but it didn't"
-        );
+        Executable e = () -> {RoleAssigner.setRole(user, badRole);};
+        Throwable thrown = assertThrows(IllegalArgumentException.class, e, "Expected setRole() to throw, but it didn't");
         assertTrue(thrown.getMessage().contains("newRoleObject returns unknown role type"));    //we want this exact error message
     }
 
@@ -129,11 +126,8 @@ public class roleAssignmentTest {
     public void setRoleMissingDependency(){
         User user = new User("user", "1234");
         Member member = new Member("John Doe", "12345678", "0000", "City Town Street 1", "RealMail@mailService.com");
-        Throwable thrown = assertThrows(
-           IllegalArgumentException.class,
-           () -> RoleAssigner.setRole(user, member),
-           "Expected setRole() to throw, but it didn't"
-        );
+        Executable e = () -> {RoleAssigner.setRole(user, member);};
+        Throwable thrown = assertThrows(IllegalArgumentException.class, e, "Expected setRole() to throw, but it didn't");
         assertTrue(thrown.getMessage().contains("newRoleObject is missing dependencies!"));
     }
 }
