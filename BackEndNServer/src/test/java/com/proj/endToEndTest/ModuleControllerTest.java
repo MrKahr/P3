96a52841303fe2f704b9de02db99e96a7ba82846
nikarnik <112962573+nikarnik@users.noreply.github.com>
2023-11-27 09:36:58 +0100

    test of moduleController using mocked http

--- BackEndNServer/src/test/java/com/proj/endToEndTest/ModuleControllerTest.java
+++ BackEndNServer/src/test/java/com/proj/endToEndTest/ModuleControllerTest.java
@@ -1,6 +1,5 @@
-package com.proj.integrationTest;
+package com.proj.endToEndTest;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,15 +8,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
 
 import com.proj.function.ModuleManager;
 import com.proj.model.session.Module;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // https://www.springcloud.io/post/2022-03/spring-boot-integration-testing-mysql-crud-rest-api-tutorial/#gsc.tab=0
@@ -37,59 +36,62 @@ public class ModuleControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    public void requestAddModule() {
-        try { /*
-               * Add module with name "Name", description "desc is desc" and levelRange
-               * "01-02" through mocked PUT request
-               */
+    public void requestAddModule() { // Add module through mocked PUT request
+        try {
+            String name = "The Circle of Life";
+            String description = "I am going to change.";
+            String levelRange = "01-02";
             ResultActions response = mockMvc
-                    .perform(put("/module/add?name=Name&description=desc is desc&levelRange=01-02"));
+                    .perform(put("/module/add?name=" + name + "&description=" + description + "&levelRange=" + levelRange));
 
             response.andExpect(status().isOk());
 
-            //mockMvc.perform(delete("/module/removeFromDatabase?id=1"));
+            Module module = new Module(name, description, levelRange);
+            module.setId(1);
+
+            moduleManager.getModuledbHandler().delete(module);
         } catch (Exception e) {
             fail("Unexpected exception thrown requestAddModule: " + e.getMessage()); // https://www.baeldung.com/junit-fail
         }
     }
 
-    
     @Test
-    public void requestEditModule() {
+    public void requestEditModule() { // Database persists from the last test, but not in the third
         try {
-            moduleManager.createModule("Another Test Module", "It is indescribable.", "01-10");
+            moduleManager.createModule("null", "null", "01-02");
 
-            Module updateModule = new Module("New name", "New description", "02-05");
-            updateModule.setId(1);
+            Module updateModule = new Module("The Circle of Death", "I am not who I was.", "02-10");
+            updateModule.setId(2);
+            
+            assertTrue(moduleManager.getModuledbHandler().existsById(2));
 
             ResultActions response = mockMvc.perform(put("/module/editByID")
                     .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(updateModule)));
 
-            response.andExpect(status().isOk());
-            moduleManager.getModuledbHandler().delete(updateModule);
+            response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",
+                        is(updateModule.getName())))
+                .andExpect(jsonPath("$.description",
+                        is(updateModule.getDescription())))
+                .andExpect(jsonPath("$.levelRange",
+                        is(updateModule.getLevelRange())));
         } catch (Exception e) {
             fail("Unexpected exception thrown by requestEditModule: " + e.getMessage());
         }
     }
     
-
-    
     @Test
-    public void requestRemoveModule() throws Exception {
+    public void requestRemoveModule() {
         try {
-            Module module = moduleManager.createModule("I live to die", "My fear is palpable", "20-20");
-            assertTrue(moduleManager.getModuledbHandler().existsById(module.getId()));
-            String request = "/module/removeByID?id=" + module.getId();
+            Module moduleToEdit = moduleManager.createModule("I live to die", "My fear is palpable", "20-20");
 
-            ResultActions response = mockMvc.perform(delete(request));
+            ResultActions response = mockMvc.perform(delete("/module/removeByID?id=" + moduleToEdit.getId()));
             response.andExpect(status().isOk());
 
-            ArrayList<Module> modules = new ArrayList<Module>();
-            moduleManager.getModuledbHandler().findAll().forEach(modules::add);
-            assertFalse(modules.contains(module));
+            assertTrue(moduleManager.getModuledbHandler().existsById(1));
+
         } catch (Exception e) {
             fail("Unexpected exception thrown by requestRemoveModule: " + e.getMessage());
         }
     }
-    
 }
