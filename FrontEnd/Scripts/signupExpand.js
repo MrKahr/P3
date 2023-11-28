19287792edb2538ba0642f4131dbb59de63bb7a0
LouiseWolff17 <112960420+LouiseWolff17@users.noreply.github.com>
2023-11-28 14:49:03 +0100

    signUpDone

--- FrontEnd/Scripts/signupExpand.js
+++ FrontEnd/Scripts/signupExpand.js
@@ -1,8 +1,1 @@
-document.getElementById('Password').addEventListener('focus', function() {
-    let passwordConditions = document.getElementById("PasswordConditions");
-    if(passwordConditions.style.display == "none") {
-        passwordConditions.style.display = "block";
-    } else {
-        passwordConditions.style.display = "none";
-    }
-})
+
