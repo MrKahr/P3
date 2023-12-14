05bddc7093e9be39f86cf775fe35e3ea984aafea
KristianEmilWN <kewn21@student.aau.dk>
2023-12-14 14:39:38 +0100

    Create event error handling

--- BackEndNServer/src/main/resources/FrontEnd/public/js/createeventpage.js
+++ BackEndNServer/src/main/resources/FrontEnd/public/js/createeventpage.js
@@ -16,10 +16,24 @@ const createEventButton = document.getElementById("createEventButton");
 
 createEventButton.addEventListener('click', () => {
   const title = document.querySelector('#eventTitle').value;
+  if (title == null || title == ""){alert("please add a title to your event, this can be changed later")};
   const location = document.querySelector('#eventLocation').value;
+  if (location == null || location == ""){alert("please add a location to your event, this can be changed later")};
   const description = document.querySelector('#eventDescription').value;
+  if (description == null || description == ""){alert("please add a desciption to your event, this can be changed later")};
   const date = document.querySelector('#eventDate').value;
+  if (date == null || date == ""){alert("please add a date to your event, this can be changed later")};
   const maxNumberOfPlayers = document.querySelector('#eventMaxNumberOfPlayers').value;
-
-  addCalendar(title, location, description, date, maxNumberOfPlayers);
+  if (maxNumberOfPlayers == null || maxNumberOfPlayers == ""){alert("please add a player maximum to your event, this can be changed later")};
+  
+  if (title != null && location != null && description != null && date != null && maxNumberOfPlayers != null && title != "" && location != "" && description != "" && date != "" && maxNumberOfPlayers != "") {
+    try {
+        addCalendar(title, location, description, date, maxNumberOfPlayers);
+        alert("event created successfully");
+      } catch{
+        alert("event creation failed, please check your inputs");
+      }
+  }
+  
+  
 });
