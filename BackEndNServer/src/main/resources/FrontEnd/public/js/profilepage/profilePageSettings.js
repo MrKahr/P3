ec7eea1a46c57f9ac9ba4a08545846906d5639d9
MrKahr <madska21@student.aau.dk>
2023-12-16 18:48:30 +0100

    added activation and save guest info

--- BackEndNServer/src/main/resources/FrontEnd/public/js/profilepage/profilePageSettings.js
+++ BackEndNServer/src/main/resources/FrontEnd/public/js/profilepage/profilePageSettings.js
@@ -33,7 +33,35 @@ async function getUserInfo(username) {
     }
 }
 
-async function getUserEvents(){
+async function getGuestInfo(username) {
+    try {
+        const response = await fetch(`/api/${username}/getGuestInfo`, {
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache"
+        });
+        // Return response once promise has been resolved 
+        return await JSON.parse(await response.text());
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function setGuestInfo(username, newInfo) {
+    try {
+        const response = await fetch(`/api/${username}/${newInfo}`, {
+            method: "PUT",
+            mode: "cors",
+            cache: "no-cache"
+        });
+        // Return response once promise has been resolved 
+        return await response.text();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getUserEvents() {
 
 }
 
@@ -86,32 +114,111 @@ function inputMemberInfo(userinfo) {
     });
 }
 
-async function saveGuestInfo() { };
+async function saveGuestInfo() {
+    // Save it to a user's guest object
+    let username = await getCurrentUserName();
+    try {
+        // Get info in infobox
+        let currentTextArea = document.getElementById("profileText");
+        let guestInfo = {
+            characterInfo: currentTextArea.value,
+            "@class":".Guest"
+        };
+        // Jacksonserializer complains about missing id attribut @class - fix: https://stackoverflow.com/questions/31665620/is-jacksons-jsonsubtypes-still-necessary-for-polymorphic-deserialization
+        const response = await fetch(`/api/${username}/saveGuestInfo`,
+            {
+                method: "PUT",
+                mode: "cors",
+                cache: "no-cache",
+                headers: {
+                    "Content-Type": "application/json" // Set type to JSON
+                },
+                body: JSON.stringify(guestInfo)
+            })
+            let currentInfoContainer = document.getElementsByClassName("Save Changes Message");
+            currentInfoContainer[0].innerText = "Save successful";
+            currentInfoContainer[0].visibility = "visible";
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 async function saveMemberInfo() { };
 
 function requestMembership() { };
 
-function changeActivationStatus(){
-    let currentButtonStatus = document.getElementById("activationButton").value; 
-    let promptmsg = "You are about to " + currentButtonStatus + "are you sure you want to do this?";
+async function changeActivationStatus() {
+    let currentButton = document.getElementById("activationButton");
+
+    // Setup prompt to make sure user deactivates/activates account correctly
+    let promptmsg = "You are about to" + currentButton.innerText + ". Your account can be restored in the next six months. \n Are you sure you want to " + currentButton.innerText + " [yes/no]";
     let activationPrompt = prompt(promptmsg);
-    currentButtonStatus = "HELLO";
-    if(activationPrompt === "yes"){
-        console.log(currentButtonStatus);
-        if(currentButtonStatus === "Deactivate Account"){
-            currentButtonStatus = "Activate Account";
+
+    let reponseMessage;
+
+    if (activationPrompt === "yes") {
+        // Check whether button is currently an activation or deactivation button
+        if (currentButton.innerText.split(" ")[0] === "Deactivate") {
+            reponseMessage = await deactivateAccount();
         } else {
-            currentButtonStatus = "Deactivate Account";
+            reponseMessage = await activateAccount();
         }
+
+        changeActivationButtonText(currentButton);
+
+        // Display message to user 
+        let statusMessage = document.getElementsByClassName("Activation Message");
+        statusMessage[0].visibility = "visible";
+        statusMessage[0].innerText = reponseMessage;
     } else {
         console.warn("Operation unsuccessful!");
     }
 }
 
-function generatePageContent(){
+function changeActivationButtonText(activationButton) {
+    if (activationButton.innerText === "Deactivate Account") {
+        activationButton.innerText = "Activate Account";
+    } else {
+        activationButton.innerText = "Deactivate Account";
+    }
+}
+
+async function deactivateAccount() {
+    let username = await getCurrentUserName();
+
+    try {
+        const response = await fetch(`/api/${username}/deactivate`, {
+            method: "PUT",
+            mode: "cors",
+            cache: "no-cache"
+        });
+        // Return response once promise has been resolved 
+        return await response.text();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function activateAccount() {
+    let username = await getCurrentUserName();
+    try {
+        const response = await fetch(`/api/${username}/reactivate`, {
+            method: "PUT",
+            mode: "cors",
+            cache: "no-cache"
+        });
+        // Return response once promise has been resolved 
+        return await response.text();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function generatePageContent() {
     inputUserInfo();
     document.getElementById("activationButton").addEventListener("click", changeActivationStatus);
+    document.getElementById("guestInfoSave").addEventListener("click", saveGuestInfo);
 }
 
 generatePageContent();
