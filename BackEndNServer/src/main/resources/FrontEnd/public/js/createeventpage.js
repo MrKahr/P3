66e786d0996dd0e32bf6e48ed77e7b999bec31dc
KristianEmilWN <kewn21@student.aau.dk>
2023-12-18 11:48:59 +0100

    max number of players validation

--- BackEndNServer/src/main/resources/FrontEnd/public/js/createeventpage.js
+++ BackEndNServer/src/main/resources/FrontEnd/public/js/createeventpage.js
@@ -16,17 +16,18 @@ const createEventButton = document.getElementById("createEventButton");
 
 createEventButton.addEventListener('click', () => {
   const title = document.querySelector('#eventTitle').value;
-  if (title == null || title == ""){alert("please add a title to your event, this can be changed later")};
+  //TODO: validate max length
+  if (title == null || title == ""){alert("please add a valid title (max 40 characters) to your event, this can be changed later")};
   const location = document.querySelector('#eventLocation').value;
   if (location == null || location == ""){alert("please add a location to your event, this can be changed later")};
   const description = document.querySelector('#eventDescription').value;
   if (description == null || description == ""){alert("please add a desciption to your event, this can be changed later")};
   const date = document.querySelector('#eventDate').value;
-  if (date == null || date == "" || date < Date.now()){alert("please add a valid date to your event, this can be changed later")};
+  if (date == null || date == ""){alert("please add a valid date to your event, this can be changed later")};
   const maxNumberOfPlayers = document.querySelector('#eventMaxNumberOfPlayers').value;
-  if (maxNumberOfPlayers == null || maxNumberOfPlayers == "" || maxNumberOfPlayers > 7){alert("please add a player maximum (max 7) to your event, this can be changed later")};
+  if (maxNumberOfPlayers == null || maxNumberOfPlayers == "" || maxNumberOfPlayers > 7){alert("please add valid a player maximum (max 7) to your event, this can be changed later")};
   
-  if (title != null && location != null && description != null && date != null && maxNumberOfPlayers != null && title != "" && location != "" && description != "" && date != "" && maxNumberOfPlayers != "") {
+  if (title != null && location != null && description != null && date != null && maxNumberOfPlayers != null && title != "" && location != "" && description != "" && date != "" && maxNumberOfPlayers != "" && maxNumberOfPlayers <= 7) {
     try {
         addCalendar(title, location, description, date, maxNumberOfPlayers);
         alert("event created successfully");
@@ -34,6 +35,5 @@ createEventButton.addEventListener('click', () => {
         alert("event creation failed, please check your inputs");
       }
   }
-  
   
 });
